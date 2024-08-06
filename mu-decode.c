b195eeb72414bf09d890caab02040572a55fc8bc
JackCotter <79673714+JackCotter@users.noreply.github.com>
2024-08-06 15:30:19 -0700

    Implement coding standards (#14)
    * implemented Barr-c 1.3, 1.4, 1.6
    
    * MISRA 15.7 Barr-C 5.2
    
    * Implemented Barr-C 7.1
    
    * move to unsinged integers

--- mu-decode.c
+++ mu-decode.c
@@ -6,103 +6,151 @@
 // WAV file header format
 typedef struct {
     char riff[4];
-    uint32_t chunkSize;
+    uint32_t chunk_size;
     char wave[4];
     char fmt[4];
-    uint32_t subChunk1Size;
-    uint16_t audioFormat;
-    uint16_t numChannels;
-    uint32_t sampleRate;
-    uint32_t byteRate;
-    uint16_t blockAlign;
-    uint16_t bitsPerSample;
+    uint32_t sub_chunk_1_size;
+    uint16_t audio_format;
+    uint16_t num_channels;
+    uint32_t sample_rate;
+    uint32_t byte_rate;
+    uint16_t block_align;
+    uint16_t bits_per_sample;
     uint32_t data;
-    uint32_t dataSize;
+    uint32_t data_size;
 } WAVHeader;
 
-int8_t sign (int8_t sample) {
+uint8_t sign (uint8_t sample) {
 	//return the sign of the sample: 1 (true) if positive, 0 (false) if negative. 
-	if (sample & 1 << 7) {
+	if (sample & (1 << 7)) 
+	{
 		return 1;
+	}
+	else
+	{
 	}
 	return 0;
 }
 
-int8_t magnitude (int8_t sample) {
+uint8_t magnitude (uint8_t sample) {
 	return sample & 0x7F;
 }
 
-int16_t codeword_expansion (int8_t sample_magnitude, int8_t sign) {
-	int16_t chord, step, codeword_tmp;
+uint16_t codeword_expansion (uint8_t sample_magnitude, uint8_t sign) {
+	uint16_t chord, step, codeword_tmp;
 
-	if ((sample_magnitude & (0x7 << 4)) == 0x70) {
+	if ((sample_magnitude & (0x7 << 4)) == 0x70) 
+	{
 		chord = 0x1 << 12;
 		step = (sample_magnitude & 0xF);
 	       	codeword_tmp = (sign << 15) | (chord | step << 8 | 0x1 << 7) << 2;
-	       	return (int16_t) codeword_tmp;
+	       	return (uint16_t) codeword_tmp; // Works with codeword_tmp as it is already 16 bits
 	}
-	if ((sample_magnitude & (0x6 << 4)) == 0x60) {
+	else
+	{
+	}
+	if ((sample_magnitude & (0x6 << 4)) == 0x60) 
+	{
 		chord = 0x1 << 11;
 		step = (sample_magnitude & 0xF);
 	       	codeword_tmp = (sign << 15) | (chord | step << 7 | 0x1 << 6) << 2;
-	       	return (int16_t) codeword_tmp;
+	       	return (uint16_t) codeword_tmp; // Works with codeword_tmp as it is already 16 bits
 	} 
-	if ((sample_magnitude & (0x5 << 4)) == 0x50) {
+	else
+	{
+	}
+	if ((sample_magnitude & (0x5 << 4)) == 0x50) 
+	{
 		chord = 0x1 << 10;
 		step = (sample_magnitude & 0xF);
 	       	codeword_tmp = (sign << 15) | (chord | step << 6 | 0x1 << 5) << 2;
-	       	return (int16_t) codeword_tmp;
+	       	return (uint16_t) codeword_tmp; // Works with codeword_tmp as it is already 16 bits
 	} 
-	if ((sample_magnitude & (0x4 << 4)) == 0x40) {
+	else
+	{
+	}
+	if ((sample_magnitude & (0x4 << 4)) == 0x40) 
+	{
 		chord = 0x1 << 9;
 		step = (sample_magnitude & 0xF);
 	       	codeword_tmp = (sign << 15) | (chord | step << 5 | 0x1 << 4) << 2;
-	       	return (int16_t) codeword_tmp;
+	       	return (uint16_t) codeword_tmp; // Works with codeword_tmp as it is already 16 bits
 	}
-	if ((sample_magnitude & (0x3 << 4)) == 0x30) {
+	else
+	{
+	}
+	if ((sample_magnitude & (0x3 << 4)) == 0x30) 
+	{
 		chord = 0x1 << 8;
 		step = (sample_magnitude & 0xF);
 	       	codeword_tmp = (sign << 15) | (chord | step << 4 | 0x1 << 3) << 2;
-	       	return (int16_t) codeword_tmp;
+	       	return (uint16_t) codeword_tmp; // Works with codeword_tmp as it is already 16 bits
 	}
-	if ((sample_magnitude & (0x2 << 4)) == 0x20) {
+	else
+	{
+	}
+	if ((sample_magnitude & (0x2 << 4)) == 0x20) 
+	{
 		chord = 0x1 << 7;
 		step = (sample_magnitude & 0xF);
 	       	codeword_tmp = (sign << 15) | (chord | step << 3 | 0x1 << 2) << 2;
-	       	return (int16_t) codeword_tmp;
+	       	return (uint16_t) codeword_tmp; // Works with codeword_tmp as it is already 16 bits
 	}
-	if ((sample_magnitude & (0x1 << 4)) == 0x10) {
+	else
+	{
+	}
+	if ((sample_magnitude & (0x1 << 4)) == 0x10) 
+	{
 		chord = 0x1 << 6;
 		step = (sample_magnitude & 0xF);
 	       	codeword_tmp = (sign << 15) | (chord | step << 2 | 0x1 << 1) << 2;
-	       	return (int16_t) codeword_tmp;
+	       	return (uint16_t) codeword_tmp; // Works with codeword_tmp as it is already 16 bits
 	}
-	if (sample_magnitude & (0x7 << 4) == 0) {
+	else
+	{
+	}
+	if (sample_magnitude & (0x7 << 4) == 0) 
+	{
 		chord = 0x1 << 5;
 		step = (sample_magnitude & 0xF);
 	       	codeword_tmp = (sign << 15) | (chord | step << 1 | 0x1) << 2;
-	       	return (int16_t) codeword_tmp;
+	       	return (uint16_t) codeword_tmp; // Works with codeword_tmp as it is already 16 bits
+	}
+	else
+	{
 	}
 	return 0;
 }
 
 int main(int argc, char *argv[]) {
-	if (argc != 2) {
+	if (argc != 2) 
+	{
 		printf("Usage: %s <path/to/wav/file>", argv[0]);
 		return 1;
 	}
+	else
+	{
+	}
 
 	FILE* file = fopen(argv[1], "rb");
-	if (!file) {
+	if (!file) 
+	{
 		printf("Failed to open file");
+	}
+	else
+	{
 	}
 
 	// open the output file for writing
 	FILE *output_file = fopen("decompressed_out.wav", "wb");
-	if (!output_file) {
+	if (!output_file) 
+	{
 		fprintf(stderr, "Error opening output file\n");
 		fclose(file);
 		return 0;
+	}
+	else
+	{
 	}
 
 	// read and modify the WAV file header
@@ -114,35 +162,40 @@ int main(int argc, char *argv[]) {
 	out_header = in_header;
 
 	// Check if the input file is a valid WAV file
-	if (memcmp(in_header.riff, "RIFF", 4) != 0 || memcmp(in_header.wave, "WAVE", 4) != 0) {
+	if (memcmp(in_header.riff, "RIFF", 4) != 0 || memcmp(in_header.wave, "WAVE", 4) != 0) 
+	{
 		fprintf(stderr, "Invalid WAV file\n");
 		fclose(file);
 		fclose(output_file);
 		return 0;
 	}
+	else
+	{
+	}
 	// alter header fields for compressed data
-	out_header.bitsPerSample = 16;
-	out_header.byteRate = out_header.sampleRate * out_header.numChannels << 1; // this calculation is sampleRate * numChannels * (bitsPerSample / 8) simplfied for 8 bit sample rate
-	out_header.blockAlign = out_header.numChannels << 1; // this calculation is numChannels * bitsPerSample / 8 simplified for 8 bit sample rate
-	out_header.dataSize = out_header.dataSize << 1; //this can be reorganized to remove direct dependancies
-	out_header.chunkSize = 36 + out_header.dataSize;
+	out_header.bits_per_sample = 16;
+	out_header.byte_rate = out_header.sample_rate * out_header.num_channels << 1;
+	out_header.block_align = out_header.num_channels << 1; 
+	out_header.data_size = out_header.data_size << 1; 
+	out_header.chunk_size = 36 + out_header.data_size;
 
 	fwrite(&out_header, sizeof(WAVHeader), 1, output_file);
 
 	// reading logic will need to be changed
-	int loop_limit = in_header.dataSize >> 1;
+	int loop_limit = in_header.data_size >> 1;
 	fseek(file, 0, in_header.data);
-	for(int i = 0; i < loop_limit; i++) {
-		int8_t byte = fgetc(file);
-		int8_t byte_2 = fgetc(file);
+	for(int index = 0; index < loop_limit; index++)  
+	{
+		uint8_t byte = fgetc(file);
+		uint8_t byte_2 = fgetc(file);
 
-		int8_t sig = sign(byte);
-		int8_t mag = magnitude(byte);
-		int8_t sig_2 = sign(byte_2);
-		int8_t mag_2 = magnitude(byte_2);
+		uint8_t sig = sign(byte);
+		uint8_t mag = magnitude(byte);
+		uint8_t sig_2 = sign(byte_2);
+		uint8_t mag_2 = magnitude(byte_2);
 		//compress
-		int16_t data_point = codeword_expansion(mag, sig);
-		int16_t data_point_2 = codeword_expansion(mag_2, sig_2);
+		uint16_t data_point = codeword_expansion(mag, sig);
+		uint16_t data_point_2 = codeword_expansion(mag_2, sig_2);
 		//write to output file
 		fwrite(&data_point, sizeof(data_point), 1, output_file);
 		fwrite(&data_point_2, sizeof(data_point_2), 1, output_file);
--- mu-encode.c
+++ mu-encode.c
@@ -6,105 +6,152 @@
 // WAV file header format
 typedef struct {
     char riff[4];
-    uint32_t chunkSize;
+    uint32_t chunk_size; //CODING STANDARDS: No variable names should include upper case letters as per Barr-C section 7.1
     char wave[4];
     char fmt[4];
-    uint32_t subChunk1Size;
-    uint16_t audioFormat;
-    uint16_t numChannels;
-    uint32_t sampleRate;
-    uint32_t byteRate;
-    uint16_t blockAlign;
-    uint16_t bitsPerSample;
+    uint32_t sub_chunk_1_size;
+    uint16_t audio_format;
+    uint16_t num_channels;
+    uint32_t sample_rate;
+    uint32_t byte_rate;
+    uint16_t block_align;
+    uint16_t bits_per_sample;
     uint32_t data;
-    uint32_t dataSize;
+    uint32_t data_size;
 } WAVHeader;
 
-int16_t sign (int16_t sample) {
-	//return the sign of the sample: 1 (true) if positive, 0 (false) if negative. 
-	if (sample & 1 << 15) {
+uint16_t sign (uint16_t sample) {
+	// return the sign of the sample: 1 (true) if positive, 0 (false) if negative. 
+	if (sample & (1 << 15)) 
+	{ 				// CODING STANDARDS: Braces must appear by itself on the line below the start of the block as per Barr-C section 1.3.
 		return 1;
+	}
+	else
+	{         // CODING STANDARDS: An if statement should be terminiated with an else statement as per MISRA-C 15.7.
 	}
 	return 0;
 }
 
-int16_t magnitude (int16_t sample) {
+uint16_t magnitude (uint16_t sample) {
 	return sample & 0x7FFF;
 }
 
-int8_t codeword_compression (int16_t sample_magnitude, int16_t sign) {
-	int16_t chord, step;
-	int8_t codeword_tmp = 0;
+uint8_t codeword_compression (uint16_t sample_magnitude, uint16_t sign) {
+	uint16_t chord, step; // CODING STANDARDS: bitwise operations should not be performed on signed integers, use unsigned. Barr-C section 5.3.
+	uint8_t codeword_tmp = 0;
 
-	if (sample_magnitude & (1 << 12)) {
-		//12th bit is set therefore 8th chord
+	if (sample_magnitude & (1 << 12))    // CODING STANDARDS: So not rely on operator precedence rules, use parentheses to ensure proper execution of code as per Barr-C section 1.4. 
+	{
 		chord = 0x7;
 		step = (sample_magnitude >> 8) & 0xF;
 		codeword_tmp = (sign << 7) | (chord << 4) | step;
-		return (int8_t) codeword_tmp;
+		return (uint8_t) codeword_tmp; // Works with codeword_tmp as it is already uint8
+		// CODING STANDARDS: Provide comments for each cast in your code as per Barr-C section 1.6
 	}
-	if (sample_magnitude & (1 << 11)) {
+	else
+	{
+	}
+	if (sample_magnitude & (1 << 11)) 
+	{
 		chord = 0x6;
 		step = (sample_magnitude >> 7) & 0xF;
 		codeword_tmp = (sign << 7) | (chord << 4) | step;
-		return (int8_t) codeword_tmp;
+		return (uint8_t) codeword_tmp; // Works with codeword_tmp as it is already 8 bits
 	}
-	if (sample_magnitude & (1 << 10)) {
+	else
+	{
+	}
+	if (sample_magnitude & (1 << 10)) 
+	{
 		chord = 0x5;
 		step = (sample_magnitude >> 6) & 0xF;
 		codeword_tmp = (sign << 7) | (chord << 4) | step;
-		return (int8_t) codeword_tmp;
+		return (uint8_t) codeword_tmp; // Works with codeword_tmp as it is already 8 bits
 	}
-	if (sample_magnitude & (1 << 9)) {
+	else
+	{
+	}
+	if (sample_magnitude & (1 << 9)) 
+	{
 		chord = 0x4;
 		step = (sample_magnitude >> 5) & 0xF;
 		codeword_tmp = (sign << 7) | (chord << 4) | step;
-		return (int8_t) codeword_tmp;
+		return (uint8_t) codeword_tmp; // Works with codeword_tmp as it is already 8 bits
 	}
-	if (sample_magnitude & (1 << 8)) {
+	else
+	{
+	}
+	if (sample_magnitude & (1 << 8)) 
+	{
 		chord = 0x3;
 		step = (sample_magnitude >> 4) & 0xF;
 		codeword_tmp = (sign << 7) | (chord << 4) | step;
-		return (int8_t) codeword_tmp;
+		return (uint8_t) codeword_tmp; // Works with codeword_tmp as it is already 8 bits
 	}
-	if (sample_magnitude & (1 << 7)) {
+	else
+	{
+	}
+	if (sample_magnitude & (1 << 7)) 
+	{
 		chord = 0x2;
 		step = (sample_magnitude >> 3) & 0xF;
 		codeword_tmp = (sign << 7) | (chord << 4) | step;
-		return (int8_t) codeword_tmp;
+		return (uint8_t) codeword_tmp; // Works with codeword_tmp as it is already 8 bits
 	}
-	if (sample_magnitude & (1 << 6)) {
+	else
+	{
+	}
+	if (sample_magnitude & (1 << 6)) 
+	{
 		chord = 0x1;
 		step = (sample_magnitude >> 2) & 0xF;
 		codeword_tmp = (sign << 7) | (chord << 4) | step;
-		return (int8_t) codeword_tmp;
+		return (uint8_t) codeword_tmp; // Works with codeword_tmp as it is already uint8
 	}
-	if (sample_magnitude & (1 << 5)) {
+	else
+	{
+	}
+	if (sample_magnitude & (1 << 5)) 
+	{
 		chord = 0x0;
 		step = (sample_magnitude >> 1) & 0xF;
 		codeword_tmp = (sign << 7) | (chord << 4) | step;
-		return (int8_t) codeword_tmp;
+		return (uint8_t) codeword_tmp; // Works with codeword_tmp as it is already uint8
+	}
+	else
+	{
 	}
 	return 0;
 }
 
 int main(int argc, char *argv[]) {
-	if (argc != 2) {
+	if (argc != 2) 
+	{
 		printf("Usage: %s <path/to/wav/file>", argv[0]);
 		return 1;
 	}
-
-	FILE* file = fopen(argv[1], "rb");
-	if (!file) {
-		printf("Failed to open file");
+	else
+	{
 	}
 
-	// open the output file for writing
+	FILE* file = fopen(argv[1], "rb");
+	if (!file) 
+	{
+		printf("Failed to open file");
+	}
+	else
+	{
+	}
+
 	FILE *output_file = fopen("out.wav", "wb");
-	if (!output_file) {
+	if (!output_file) 
+	{
 		fprintf(stderr, "Error opening output file\n");
 		fclose(file);
 		return 0;
+	}
+	else
+	{
 	}
 
 	// read and modify the WAV file header
@@ -116,18 +163,22 @@ int main(int argc, char *argv[]) {
 	out_header = in_header;
 
 	// Check if the input file is a valid WAV file
-	if (memcmp(in_header.riff, "RIFF", 4) != 0 || memcmp(in_header.wave, "WAVE", 4) != 0) {
+	if (memcmp(in_header.riff, "RIFF", 4) != 0 || memcmp(in_header.wave, "WAVE", 4) != 0) 
+	{
 		fprintf(stderr, "Invalid WAV file\n");
 		fclose(file);
 		fclose(output_file);
 		return 0;
 	}
+	else
+	{
+	}
 	// alter header fields for compressed data
-	out_header.bitsPerSample = 8;
-	out_header.byteRate = out_header.sampleRate * out_header.numChannels; // this calculation is sampleRate * numChannels * (bitsPerSample / 8) simplfied for 8 bit sample rate
-	out_header.blockAlign = out_header.numChannels; // this calculation is numChannels * bitsPerSample / 8 simplified for 8 bit sample rate
-	out_header.dataSize = out_header.dataSize >> 1; //this can be reorganized to remove direct dependancies
-	out_header.chunkSize = 36 + out_header.dataSize;
+	out_header.bits_per_sample = 8;
+	out_header.byte_rate = out_header.sample_rate * out_header.num_channels; 
+	out_header.block_align = out_header.num_channels; 
+	out_header.data_size = out_header.data_size >> 1; 
+	out_header.chunk_size = 36 + out_header.data_size;
 
 	fwrite(&out_header, sizeof(WAVHeader), 1, output_file);
 
@@ -135,24 +186,24 @@ int main(int argc, char *argv[]) {
 	int bits_in_buffer = 0;
 	int byte;
 	fseek(file, 0, in_header.data);
-	int loopIterations = in_header.dataSize >> 2;
-	for(int i = 0; i < loopIterations; i++) {
-		//read 2 bytes 1 byte at a time
-		int16_t byte2 = fgetc(file);
-		int16_t byte1 = fgetc(file);
-		int16_t byte2_2 = fgetc(file);
-		int16_t byte1_2 = fgetc(file);
+	int loopIterations = in_header.data_size >> 2;
+	for(int index = 0; index < loopIterations; index++) //CODING STANDARDS: No variable name should be shorted than 3 characters, including loop counters as per Barr-C 7.1.
+	{
+		uint16_t byte2 = fgetc(file); //CODING STANDARDS: Whenever the width of an integer matters in a program, a fixed width data type should be used as per Barr-C 5.2.
+		uint16_t byte1 = fgetc(file);
+		uint16_t byte2_2 = fgetc(file);
+		uint16_t byte1_2 = fgetc(file);
 		// combine the 2 bytes into a 16 bit integer
-		int16_t data_point = (byte1 << 8 & 0x8000 | ((byte1 & 0x7F) << 6) | (byte2 >> 2));
-		int16_t data_point_2 = (byte1_2 << 8 & 0x8000 | ((byte1_2 & 0x7F) << 6) | (byte2_2 >> 2));
+		uint16_t data_point = (byte1 << 8 & 0x8000 | ((byte1 & 0x7F) << 6) | (byte2 >> 2));
+		uint16_t data_point_2 = (byte1_2 << 8 & 0x8000 | ((byte1_2 & 0x7F) << 6) | (byte2_2 >> 2));
 		//extract sign and magnitude of the data point
-		int16_t sig = sign(data_point);
-		int16_t mag = magnitude(data_point);
-		int16_t sig_2 = sign(data_point_2);
-		int16_t mag_2 = magnitude(data_point_2);
+		uint16_t sig = sign(data_point);
+		uint16_t mag = magnitude(data_point);
+		uint16_t sig_2 = sign(data_point_2);
+		uint16_t mag_2 = magnitude(data_point_2);
 		//compress
-		int8_t output_data_point = codeword_compression(mag, sig);
-		int8_t output_data_point_2 = codeword_compression(mag_2, sig_2);
+		uint8_t output_data_point = codeword_compression(mag, sig);
+		uint8_t output_data_point_2 = codeword_compression(mag_2, sig_2);
 		//write to output file
 		fwrite(&output_data_point, sizeof(output_data_point), 1, output_file);
 		fwrite(&output_data_point_2, sizeof(output_data_point_2), 1, output_file);
