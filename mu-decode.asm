f8cbff06244b4567ead0878e03059c5628e4670a
Jack <jackwcotter101@gmail.com>
2024-08-14 22:36:54 -0700

    compiled using -O3 flag and created asm files

--- mu-decode.asm
+++ mu-decode.asm
@@ -6,7 +6,7 @@
 	.eabi_attribute 24, 1
 	.eabi_attribute 25, 1
 	.eabi_attribute 26, 2
-	.eabi_attribute 30, 6
+	.eabi_attribute 30, 2
 	.eabi_attribute 34, 1
 	.eabi_attribute 18, 4
 	.file	"mu-decode.c"
@@ -19,26 +19,10 @@
 	.fpu vfpv3-d16
 	.type	sign, %function
 sign:
-	@ args = 0, pretend = 0, frame = 8
-	@ frame_needed = 1, uses_anonymous_args = 0
+	@ args = 0, pretend = 0, frame = 0
+	@ frame_needed = 0, uses_anonymous_args = 0
 	@ link register save eliminated.
-	str	fp, [sp, #-4]!
-	add	fp, sp, #0
-	sub	sp, sp, #12
-	mov	r3, r0
-	strb	r3, [fp, #-5]
-	ldrsb	r3, [fp, #-5]
-	cmp	r3, #0
-	bge	.L2
-	mov	r3, #1
-	b	.L3
-.L2:
-	mov	r3, #0
-.L3:
-	mov	r0, r3
-	add	sp, fp, #0
-	@ sp needed
-	ldr	fp, [sp], #4
+	lsr	r0, r0, #7
 	bx	lr
 	.size	sign, .-sign
 	.align	2
@@ -48,21 +32,10 @@ sign:
 	.fpu vfpv3-d16
 	.type	magnitude, %function
 magnitude:
-	@ args = 0, pretend = 0, frame = 8
-	@ frame_needed = 1, uses_anonymous_args = 0
+	@ args = 0, pretend = 0, frame = 0
+	@ frame_needed = 0, uses_anonymous_args = 0
 	@ link register save eliminated.
-	str	fp, [sp, #-4]!
-	add	fp, sp, #0
-	sub	sp, sp, #12
-	mov	r3, r0
-	strb	r3, [fp, #-5]
-	ldrb	r3, [fp, #-5]
-	and	r3, r3, #127
-	sxtb	r3, r3
-	mov	r0, r3
-	add	sp, fp, #0
-	@ sp needed
-	ldr	fp, [sp], #4
+	and	r0, r0, #127
 	bx	lr
 	.size	magnitude, .-magnitude
 	.align	2
@@ -72,239 +45,20 @@ magnitude:
 	.fpu vfpv3-d16
 	.type	codeword_expansion, %function
 codeword_expansion:
-	@ args = 0, pretend = 0, frame = 16
-	@ frame_needed = 1, uses_anonymous_args = 0
+	@ args = 0, pretend = 0, frame = 0
+	@ frame_needed = 0, uses_anonymous_args = 0
 	@ link register save eliminated.
-	str	fp, [sp, #-4]!
-	add	fp, sp, #0
-	sub	sp, sp, #20
-	mov	r3, r0
-	mov	r2, r1
-	strb	r3, [fp, #-13]
-	mov	r3, r2
-	strb	r3, [fp, #-14]
-	ldrb	r3, [fp, #-13]	@ zero_extendqisi2
-	and	r3, r3, #112
-	cmp	r3, #112
-	bne	.L7
-	mov	r3, #4096
-	strh	r3, [fp, #-6]	@ movhi
-	ldrb	r3, [fp, #-13]	@ zero_extendqisi2
-	sxth	r3, r3
-	and	r3, r3, #15
-	strh	r3, [fp, #-8]	@ movhi
-	ldrsb	r3, [fp, #-14]
-	lsl	r3, r3, #15
-	sxth	r2, r3
-	ldrsh	r1, [fp, #-6]
-	ldrsh	r3, [fp, #-8]
-	lsl	r3, r3, #8
-	orr	r3, r1, r3
-	lsl	r3, r3, #2
-	sxth	r3, r3
-	orr	r3, r3, #512
-	sxth	r3, r3
-	orr	r3, r2, r3
-	strh	r3, [fp, #-10]	@ movhi
-	ldrsh	r3, [fp, #-10]
-	b	.L8
-.L7:
-	ldrb	r3, [fp, #-13]	@ zero_extendqisi2
-	and	r3, r3, #96
-	cmp	r3, #96
-	bne	.L9
-	mov	r3, #2048
-	strh	r3, [fp, #-6]	@ movhi
-	ldrb	r3, [fp, #-13]	@ zero_extendqisi2
-	sxth	r3, r3
-	and	r3, r3, #15
-	strh	r3, [fp, #-8]	@ movhi
-	ldrsb	r3, [fp, #-14]
-	lsl	r3, r3, #15
-	sxth	r2, r3
-	ldrsh	r1, [fp, #-6]
-	ldrsh	r3, [fp, #-8]
-	lsl	r3, r3, #7
-	orr	r3, r1, r3
-	lsl	r3, r3, #2
-	sxth	r3, r3
-	orr	r3, r3, #256
-	sxth	r3, r3
-	orr	r3, r2, r3
-	strh	r3, [fp, #-10]	@ movhi
-	ldrsh	r3, [fp, #-10]
-	b	.L8
-.L9:
-	ldrb	r3, [fp, #-13]	@ zero_extendqisi2
-	and	r3, r3, #80
-	cmp	r3, #80
-	bne	.L10
-	mov	r3, #1024
-	strh	r3, [fp, #-6]	@ movhi
-	ldrb	r3, [fp, #-13]	@ zero_extendqisi2
-	sxth	r3, r3
-	and	r3, r3, #15
-	strh	r3, [fp, #-8]	@ movhi
-	ldrsb	r3, [fp, #-14]
-	lsl	r3, r3, #15
-	sxth	r2, r3
-	ldrsh	r1, [fp, #-6]
-	ldrsh	r3, [fp, #-8]
-	lsl	r3, r3, #6
-	orr	r3, r1, r3
-	lsl	r3, r3, #2
-	sxth	r3, r3
-	orr	r3, r3, #128
-	sxth	r3, r3
-	orr	r3, r2, r3
-	strh	r3, [fp, #-10]	@ movhi
-	ldrsh	r3, [fp, #-10]
-	b	.L8
-.L10:
-	ldrb	r3, [fp, #-13]	@ zero_extendqisi2
-	and	r3, r3, #64
-	cmp	r3, #0
-	beq	.L11
-	mov	r3, #512
-	strh	r3, [fp, #-6]	@ movhi
-	ldrb	r3, [fp, #-13]	@ zero_extendqisi2
-	sxth	r3, r3
-	and	r3, r3, #15
-	strh	r3, [fp, #-8]	@ movhi
-	ldrsb	r3, [fp, #-14]
-	lsl	r3, r3, #15
-	sxth	r2, r3
-	ldrsh	r1, [fp, #-6]
-	ldrsh	r3, [fp, #-8]
-	lsl	r3, r3, #5
-	orr	r3, r1, r3
-	lsl	r3, r3, #2
-	sxth	r3, r3
-	orr	r3, r3, #64
-	sxth	r3, r3
-	orr	r3, r2, r3
-	strh	r3, [fp, #-10]	@ movhi
-	ldrsh	r3, [fp, #-10]
-	b	.L8
-.L11:
-	ldrb	r3, [fp, #-13]	@ zero_extendqisi2
-	and	r3, r3, #48
-	cmp	r3, #48
-	bne	.L12
-	mov	r3, #256
-	strh	r3, [fp, #-6]	@ movhi
-	ldrb	r3, [fp, #-13]	@ zero_extendqisi2
-	sxth	r3, r3
-	and	r3, r3, #15
-	strh	r3, [fp, #-8]	@ movhi
-	ldrsb	r3, [fp, #-14]
-	lsl	r3, r3, #15
-	sxth	r2, r3
-	ldrsh	r1, [fp, #-6]
-	ldrsh	r3, [fp, #-8]
-	lsl	r3, r3, #4
-	orr	r3, r1, r3
-	lsl	r3, r3, #2
-	sxth	r3, r3
-	orr	r3, r3, #32
-	sxth	r3, r3
-	orr	r3, r2, r3
-	strh	r3, [fp, #-10]	@ movhi
-	ldrsh	r3, [fp, #-10]
-	b	.L8
-.L12:
-	ldrb	r3, [fp, #-13]	@ zero_extendqisi2
-	and	r3, r3, #32
-	cmp	r3, #0
-	beq	.L13
-	mov	r3, #128
-	strh	r3, [fp, #-6]	@ movhi
-	ldrb	r3, [fp, #-13]	@ zero_extendqisi2
-	sxth	r3, r3
-	and	r3, r3, #15
-	strh	r3, [fp, #-8]	@ movhi
-	ldrsb	r3, [fp, #-14]
-	lsl	r3, r3, #15
-	sxth	r2, r3
-	ldrsh	r1, [fp, #-6]
-	ldrsh	r3, [fp, #-8]
-	lsl	r3, r3, #3
-	orr	r3, r1, r3
-	lsl	r3, r3, #2
-	sxth	r3, r3
-	orr	r3, r3, #16
-	sxth	r3, r3
-	orr	r3, r2, r3
-	strh	r3, [fp, #-10]	@ movhi
-	ldrsh	r3, [fp, #-10]
-	b	.L8
-.L13:
-	ldrb	r3, [fp, #-13]	@ zero_extendqisi2
-	and	r3, r3, #16
-	cmp	r3, #0
-	beq	.L14
-	mov	r3, #64
-	strh	r3, [fp, #-6]	@ movhi
-	ldrb	r3, [fp, #-13]	@ zero_extendqisi2
-	sxth	r3, r3
-	and	r3, r3, #15
-	strh	r3, [fp, #-8]	@ movhi
-	ldrsb	r3, [fp, #-14]
-	lsl	r3, r3, #15
-	sxth	r2, r3
-	ldrsh	r1, [fp, #-6]
-	ldrsh	r3, [fp, #-8]
-	lsl	r3, r3, #2
-	orr	r3, r1, r3
-	lsl	r3, r3, #2
-	sxth	r3, r3
-	orr	r3, r3, #8
-	sxth	r3, r3
-	orr	r3, r2, r3
-	strh	r3, [fp, #-10]	@ movhi
-	ldrsh	r3, [fp, #-10]
-	b	.L8
-.L14:
-	mov	r3, #0
-.L8:
-	mov	r0, r3
-	add	sp, fp, #0
-	@ sp needed
-	ldr	fp, [sp], #4
+	and	r3, r0, #15
+	lsl	r3, r3, #1
+	and	r0, r0, #112
+	lsl	r1, r1, #15
+	orr	r3, r3, #33
+	add	r0, r0, #2
+	orr	r0, r1, r3, lsl r0
+	uxth	r0, r0
 	bx	lr
 	.size	codeword_expansion, .-codeword_expansion
-	.section	.rodata
-	.align	2
-.LC0:
-	.ascii	"Usage: %s <path/to/wav/file>\000"
-	.align	2
-.LC1:
-	.ascii	"rb\000"
-	.align	2
-.LC2:
-	.ascii	"Failed to open file\000"
-	.align	2
-.LC3:
-	.ascii	"wb\000"
-	.align	2
-.LC4:
-	.ascii	"decompressed_out.wav\000"
-	.align	2
-.LC5:
-	.ascii	"Error opening output file\012\000"
-	.align	2
-.LC6:
-	.ascii	"RIFF\000"
-	.align	2
-.LC7:
-	.ascii	"WAVE\000"
-	.align	2
-.LC8:
-	.ascii	"Invalid WAV file\012\000"
-	.align	2
-.LC9:
-	.ascii	"%d\000"
-	.text
+	.section	.text.startup,"ax",%progbits
 	.align	2
 	.global	main
 	.syntax unified
@@ -312,190 +66,189 @@ codeword_expansion:
 	.fpu vfpv3-d16
 	.type	main, %function
 main:
-	@ args = 0, pretend = 0, frame = 120
-	@ frame_needed = 1, uses_anonymous_args = 0
-	push	{fp, lr}
-	add	fp, sp, #4
-	sub	sp, sp, #120
-	str	r0, [fp, #-120]
-	str	r1, [fp, #-124]
-	ldr	r3, [fp, #-120]
-	cmp	r3, #2
-	beq	.L16
-	ldr	r3, [fp, #-124]
-	ldr	r3, [r3]
-	mov	r1, r3
+	@ args = 0, pretend = 0, frame = 96
+	@ frame_needed = 0, uses_anonymous_args = 0
+	push	{r4, r5, r6, r7, r8, r9, lr}
+	cmp	r0, #2
+	sub	sp, sp, #100
+	mov	r3, r1
+	beq	.L6
 	movw	r0, #:lower16:.LC0
+	ldr	r1, [r1]
 	movt	r0, #:upper16:.LC0
 	bl	printf
-	mov	r3, #1
-	b	.L24
-.L16:
-	ldr	r3, [fp, #-124]
-	add	r3, r3, #4
-	ldr	r3, [r3]
+	mov	r0, #1
+.L5:
+	add	sp, sp, #100
+	@ sp needed
+	pop	{r4, r5, r6, r7, r8, r9, pc}
+.L6:
 	movw	r1, #:lower16:.LC1
+	ldr	r0, [r3, #4]
 	movt	r1, #:upper16:.LC1
-	mov	r0, r3
 	bl	fopen
-	str	r0, [fp, #-12]
-	ldr	r3, [fp, #-12]
-	cmp	r3, #0
-	bne	.L18
-	movw	r0, #:lower16:.LC2
-	movt	r0, #:upper16:.LC2
-	bl	printf
-.L18:
+	subs	r5, r0, #0
+	beq	.L19
+.L8:
 	movw	r1, #:lower16:.LC3
-	movt	r1, #:upper16:.LC3
 	movw	r0, #:lower16:.LC4
+	movt	r1, #:upper16:.LC3
 	movt	r0, #:upper16:.LC4
 	bl	fopen
-	str	r0, [fp, #-16]
-	ldr	r3, [fp, #-16]
-	cmp	r3, #0
-	bne	.L19
-	movw	r3, #:lower16:stderr
-	movt	r3, #:upper16:stderr
-	ldr	r3, [r3]
-	mov	r2, #26
-	mov	r1, #1
-	movw	r0, #:lower16:.LC5
-	movt	r0, #:upper16:.LC5
-	bl	fwrite
-	ldr	r0, [fp, #-12]
-	bl	fclose
-	mov	r3, #0
-	b	.L24
-.L19:
-	sub	r0, fp, #64
-	ldr	r3, [fp, #-12]
+	subs	r4, r0, #0
+	beq	.L20
+	movw	r6, #18770
+	mov	r3, r5
 	mov	r2, #1
 	mov	r1, #44
+	add	r0, sp, #8
 	bl	fread
-	sub	ip, fp, #108
-	sub	lr, fp, #64
-	ldmia	lr!, {r0, r1, r2, r3}
-	stmia	ip!, {r0, r1, r2, r3}
-	ldmia	lr!, {r0, r1, r2, r3}
-	stmia	ip!, {r0, r1, r2, r3}
-	ldm	lr, {r0, r1, r2}
-	stm	ip, {r0, r1, r2}
-	sub	r3, fp, #64
-	mov	r2, #4
-	movw	r1, #:lower16:.LC6
-	movt	r1, #:upper16:.LC6
-	mov	r0, r3
-	bl	memcmp
-	mov	r3, r0
-	cmp	r3, #0
-	bne	.L20
-	sub	r3, fp, #64
-	add	r3, r3, #8
-	mov	r2, #4
-	movw	r1, #:lower16:.LC7
-	movt	r1, #:upper16:.LC7
-	mov	r0, r3
-	bl	memcmp
-	mov	r3, r0
-	cmp	r3, #0
-	beq	.L21
-.L20:
+	add	ip, sp, #8
+	add	lr, sp, #52
+	ldmia	ip!, {r0, r1, r2, r3}
+	stmia	lr!, {r0, r1, r2, r3}
+	ldmia	ip!, {r0, r1, r2, r3}
+	stmia	lr!, {r0, r1, r2, r3}
+	ldm	ip, {r0, r1, r2}
+	ldr	r3, [sp, #8]
+	movt	r6, 17990
+	cmp	r3, r6
+	stm	lr, {r0, r1, r2}
+	bne	.L10
+	movw	r3, #16727
+	ldr	r2, [sp, #16]
+	movt	r3, 17750
+	cmp	r2, r3
+	beq	.L11
+.L10:
 	movw	r3, #:lower16:stderr
+	movw	r0, #:lower16:.LC6
 	movt	r3, #:upper16:stderr
 	ldr	r3, [r3]
 	mov	r2, #17
 	mov	r1, #1
-	movw	r0, #:lower16:.LC8
-	movt	r0, #:upper16:.LC8
+	movt	r0, #:upper16:.LC6
 	bl	fwrite
-	ldr	r0, [fp, #-12]
+	mov	r0, r5
 	bl	fclose
-	ldr	r0, [fp, #-16]
+	mov	r0, r4
 	bl	fclose
-	mov	r3, #0
-	b	.L24
-.L21:
-	mov	r3, #16
-	strh	r3, [fp, #-74]	@ movhi
-	ldr	r3, [fp, #-84]
-	ldrh	r2, [fp, #-86]
-	mul	r3, r2, r3
+	mov	r0, #0
+	b	.L5
+.L11:
+	mov	ip, #16
+	ldrh	r1, [sp, #74]
+	ldr	r3, [sp, #76]
+	ldr	r2, [sp, #92]
+	mul	r3, r3, r1
+	lsl	r2, r2, #1
 	lsl	r3, r3, #1
-	str	r3, [fp, #-80]
-	ldrh	r3, [fp, #-86]
-	lsl	r3, r3, #1
-	uxth	r3, r3
-	strh	r3, [fp, #-76]	@ movhi
-	ldr	r3, [fp, #-68]
-	lsl	r3, r3, #1
-	str	r3, [fp, #-68]
-	ldr	r3, [fp, #-68]
-	add	r3, r3, #36
-	str	r3, [fp, #-104]
-	sub	r0, fp, #108
-	ldr	r3, [fp, #-16]
-	mov	r2, #1
+	lsl	r1, r1, #1
+	add	lr, r2, #36
+	str	r3, [sp, #80]
+	strh	r1, [sp, #84]	@ movhi
+	mov	r3, r4
+	str	r2, [sp, #92]
 	mov	r1, #44
+	mov	r2, #1
+	add	r0, sp, #52
+	str	lr, [sp, #56]
+	strh	ip, [sp, #86]	@ movhi
 	bl	fwrite
-	ldr	r3, [fp, #-28]
-	mov	r2, r3
+	ldr	r7, [sp, #48]
+	ldr	r2, [sp, #44]
+	lsr	r7, r7, #1
 	mov	r1, #0
-	ldr	r0, [fp, #-12]
+	mov	r0, r5
 	bl	fseek
-	ldr	r3, [fp, #-28]
-	mov	r1, r3
-	movw	r0, #:lower16:.LC9
-	movt	r0, #:upper16:.LC9
-	bl	printf
-	mov	r3, #0
-	str	r3, [fp, #-8]
-	b	.L22
-.L23:
-	ldr	r0, [fp, #-12]
+	cmp	r7, #0
+	beq	.L12
+	mov	r6, #0
+.L13:
+	mov	r0, r5
 	bl	fgetc
-	mov	r3, r0
-	strb	r3, [fp, #-17]
-	ldrsb	r3, [fp, #-17]
-	mov	r0, r3
-	bl	sign
-	mov	r3, r0
-	strb	r3, [fp, #-18]
-	ldrsb	r3, [fp, #-17]
-	mov	r0, r3
-	bl	magnitude
-	mov	r3, r0
-	strb	r3, [fp, #-19]
-	ldrsb	r2, [fp, #-18]
-	ldrsb	r3, [fp, #-19]
-	mov	r1, r2
-	mov	r0, r3
-	bl	codeword_expansion
-	mov	r3, r0
-	strh	r3, [fp, #-110]	@ movhi
-	sub	r0, fp, #110
-	ldr	r3, [fp, #-16]
+	mov	r9, r0
+	mov	r0, r5
+	bl	fgetc
+	uxtb	r3, r0
+	and	r2, r3, #15
+	ubfx	r8, r0, #7, #1
+	lsl	r2, r2, #1
+	and	r3, r3, #112
+	orr	r2, r2, #33
+	add	r3, r3, #2
+	lsl	ip, r8, #15
+	orr	ip, ip, r2, lsl r3
+	mov	r2, #1
+	uxtb	r1, r9
+	and	lr, r1, #15
+	ubfx	r9, r9, #7, #1
+	lsl	lr, lr, #1
+	and	r1, r1, #112
+	orr	lr, lr, #33
+	add	r1, r1, #2
+	lsl	r9, r9, #15
+	orr	r0, r9, lr, lsl r1
+	mov	r3, r4
+	mov	r1, #2
+	strh	r0, [sp, #4]	@ movhi
+	add	r0, sp, #4
+	add	r6, r6, r2
+	strh	ip, [sp, #6]	@ movhi
+	bl	fwrite
+	mov	r3, r4
 	mov	r2, #1
 	mov	r1, #2
+	add	r0, sp, #6
 	bl	fwrite
-	ldr	r3, [fp, #-8]
-	add	r3, r3, #1
-	str	r3, [fp, #-8]
-.L22:
-	ldr	r2, [fp, #-24]
-	ldr	r3, [fp, #-8]
-	cmp	r2, r3
-	bhi	.L23
-	ldr	r0, [fp, #-16]
+	cmp	r7, r6
+	bne	.L13
+.L12:
+	mov	r0, r4
 	bl	fclose
-	ldr	r0, [fp, #-12]
+	mov	r0, r5
 	bl	fclose
-	mov	r3, #0
-.L24:
-	mov	r0, r3
-	sub	sp, fp, #4
-	@ sp needed
-	pop	{fp, pc}
+	mov	r0, #0
+	b	.L5
+.L20:
+	movw	r3, #:lower16:stderr
+	movw	r0, #:lower16:.LC5
+	movt	r3, #:upper16:stderr
+	ldr	r3, [r3]
+	mov	r2, #26
+	mov	r1, #1
+	movt	r0, #:upper16:.LC5
+	bl	fwrite
+	mov	r0, r5
+	bl	fclose
+	mov	r0, r4
+	b	.L5
+.L19:
+	movw	r0, #:lower16:.LC2
+	movt	r0, #:upper16:.LC2
+	bl	printf
+	b	.L8
 	.size	main, .-main
+	.section	.rodata.str1.4,"aMS",%progbits,1
+	.align	2
+.LC0:
+	.ascii	"Usage: %s <path/to/wav/file>\000"
+	.space	3
+.LC1:
+	.ascii	"rb\000"
+	.space	1
+.LC2:
+	.ascii	"Failed to open file\000"
+.LC3:
+	.ascii	"wb\000"
+	.space	1
+.LC4:
+	.ascii	"decompressed_out.wav\000"
+	.space	3
+.LC5:
+	.ascii	"Error opening output file\012\000"
+	.space	1
+.LC6:
+	.ascii	"Invalid WAV file\012\000"
 	.ident	"GCC: (GNU) 8.2.1 20180801 (Red Hat 8.2.1-2)"
 	.section	.note.GNU-stack,"",%progbits
--- mu-encode.asm
+++ mu-encode.asm
@@ -6,7 +6,7 @@
 	.eabi_attribute 24, 1
 	.eabi_attribute 25, 1
 	.eabi_attribute 26, 2
-	.eabi_attribute 30, 6
+	.eabi_attribute 30, 2
 	.eabi_attribute 34, 1
 	.eabi_attribute 18, 4
 	.file	"mu-encode.c"
@@ -19,26 +19,10 @@
 	.fpu vfpv3-d16
 	.type	sign, %function
 sign:
-	@ args = 0, pretend = 0, frame = 8
-	@ frame_needed = 1, uses_anonymous_args = 0
+	@ args = 0, pretend = 0, frame = 0
+	@ frame_needed = 0, uses_anonymous_args = 0
 	@ link register save eliminated.
-	str	fp, [sp, #-4]!
-	add	fp, sp, #0
-	sub	sp, sp, #12
-	mov	r3, r0
-	strh	r3, [fp, #-6]	@ movhi
-	ldrsh	r3, [fp, #-6]
-	cmp	r3, #0
-	bge	.L2
-	mov	r3, #1
-	b	.L3
-.L2:
-	mov	r3, #0
-.L3:
-	mov	r0, r3
-	add	sp, fp, #0
-	@ sp needed
-	ldr	fp, [sp], #4
+	lsr	r0, r0, #15
 	bx	lr
 	.size	sign, .-sign
 	.align	2
@@ -48,21 +32,10 @@ sign:
 	.fpu vfpv3-d16
 	.type	magnitude, %function
 magnitude:
-	@ args = 0, pretend = 0, frame = 8
-	@ frame_needed = 1, uses_anonymous_args = 0
+	@ args = 0, pretend = 0, frame = 0
+	@ frame_needed = 0, uses_anonymous_args = 0
 	@ link register save eliminated.
-	str	fp, [sp, #-4]!
-	add	fp, sp, #0
-	sub	sp, sp, #12
-	mov	r3, r0
-	strh	r3, [fp, #-6]	@ movhi
-	ldrh	r3, [fp, #-6]	@ movhi
-	ubfx	r3, r3, #0, #15
-	sxth	r3, r3
-	mov	r0, r3
-	add	sp, fp, #0
-	@ sp needed
-	ldr	fp, [sp], #4
+	ubfx	r0, r0, #0, #15
 	bx	lr
 	.size	magnitude, .-magnitude
 	.align	2
@@ -72,267 +45,31 @@ magnitude:
 	.fpu vfpv3-d16
 	.type	codeword_compression, %function
 codeword_compression:
-	@ args = 0, pretend = 0, frame = 16
-	@ frame_needed = 1, uses_anonymous_args = 0
+	@ args = 0, pretend = 0, frame = 0
+	@ frame_needed = 0, uses_anonymous_args = 0
 	@ link register save eliminated.
-	str	fp, [sp, #-4]!
-	add	fp, sp, #0
-	sub	sp, sp, #20
-	mov	r3, r0
-	mov	r2, r1
-	strh	r3, [fp, #-14]	@ movhi
-	mov	r3, r2	@ movhi
-	strh	r3, [fp, #-16]	@ movhi
-	mov	r3, #0
-	strb	r3, [fp, #-5]
-	ldrh	r3, [fp, #-14]
-	and	r3, r3, #4096
+	.syntax divided
+@ 45 "mu-encode.c" 1
+	CLZ r3, r0
+@ 0 "" 2
+	.arm
+	.syntax unified
+	sub	r2, r3, #27
+	cmp	r2, #0
+	sub	r3, r3, #26
+	rsblt	r2, r2, #0
 	cmp	r3, #0
-	beq	.L7
-	mov	r3, #7
-	strh	r3, [fp, #-8]	@ movhi
-	ldrsh	r3, [fp, #-14]
-	asr	r3, r3, #8
-	sxth	r3, r3
-	and	r3, r3, #15
-	strh	r3, [fp, #-10]	@ movhi
-	ldrsh	r3, [fp, #-16]
-	lsl	r3, r3, #7
-	sxtb	r2, r3
-	ldrsh	r3, [fp, #-8]
+	rsblt	r3, r3, #0
+	asr	r0, r0, r2
 	lsl	r3, r3, #4
-	sxtb	r3, r3
-	orr	r3, r2, r3
-	sxtb	r2, r3
-	ldrh	r3, [fp, #-10]	@ movhi
-	sxtb	r3, r3
-	orr	r3, r2, r3
-	strb	r3, [fp, #-5]
-	ldrsb	r3, [fp, #-5]
-	b	.L8
-.L7:
-	ldrh	r3, [fp, #-14]
-	and	r3, r3, #2048
-	cmp	r3, #0
-	beq	.L9
-	mov	r3, #6
-	strh	r3, [fp, #-8]	@ movhi
-	ldrsh	r3, [fp, #-14]
-	asr	r3, r3, #7
-	sxth	r3, r3
-	and	r3, r3, #15
-	strh	r3, [fp, #-10]	@ movhi
-	ldrsh	r3, [fp, #-16]
-	lsl	r3, r3, #7
-	sxtb	r2, r3
-	ldrsh	r3, [fp, #-8]
-	lsl	r3, r3, #4
-	sxtb	r3, r3
-	orr	r3, r2, r3
-	sxtb	r2, r3
-	ldrh	r3, [fp, #-10]	@ movhi
-	sxtb	r3, r3
-	orr	r3, r2, r3
-	strb	r3, [fp, #-5]
-	ldrsb	r3, [fp, #-5]
-	b	.L8
-.L9:
-	ldrh	r3, [fp, #-14]
-	and	r3, r3, #1024
-	cmp	r3, #0
-	beq	.L10
-	mov	r3, #5
-	strh	r3, [fp, #-8]	@ movhi
-	ldrsh	r3, [fp, #-14]
-	asr	r3, r3, #6
-	sxth	r3, r3
-	and	r3, r3, #15
-	strh	r3, [fp, #-10]	@ movhi
-	ldrsh	r3, [fp, #-16]
-	lsl	r3, r3, #7
-	sxtb	r2, r3
-	ldrsh	r3, [fp, #-8]
-	lsl	r3, r3, #4
-	sxtb	r3, r3
-	orr	r3, r2, r3
-	sxtb	r2, r3
-	ldrh	r3, [fp, #-10]	@ movhi
-	sxtb	r3, r3
-	orr	r3, r2, r3
-	strb	r3, [fp, #-5]
-	ldrsb	r3, [fp, #-5]
-	b	.L8
-.L10:
-	ldrh	r3, [fp, #-14]
-	and	r3, r3, #512
-	cmp	r3, #0
-	beq	.L11
-	mov	r3, #4
-	strh	r3, [fp, #-8]	@ movhi
-	ldrsh	r3, [fp, #-14]
-	asr	r3, r3, #5
-	sxth	r3, r3
-	and	r3, r3, #15
-	strh	r3, [fp, #-10]	@ movhi
-	ldrsh	r3, [fp, #-16]
-	lsl	r3, r3, #7
-	sxtb	r2, r3
-	ldrsh	r3, [fp, #-8]
-	lsl	r3, r3, #4
-	sxtb	r3, r3
-	orr	r3, r2, r3
-	sxtb	r2, r3
-	ldrh	r3, [fp, #-10]	@ movhi
-	sxtb	r3, r3
-	orr	r3, r2, r3
-	strb	r3, [fp, #-5]
-	ldrsb	r3, [fp, #-5]
-	b	.L8
-.L11:
-	ldrh	r3, [fp, #-14]
-	and	r3, r3, #256
-	cmp	r3, #0
-	beq	.L12
-	mov	r3, #3
-	strh	r3, [fp, #-8]	@ movhi
-	ldrsh	r3, [fp, #-14]
-	asr	r3, r3, #4
-	sxth	r3, r3
-	and	r3, r3, #15
-	strh	r3, [fp, #-10]	@ movhi
-	ldrsh	r3, [fp, #-16]
-	lsl	r3, r3, #7
-	sxtb	r2, r3
-	ldrsh	r3, [fp, #-8]
-	lsl	r3, r3, #4
-	sxtb	r3, r3
-	orr	r3, r2, r3
-	sxtb	r2, r3
-	ldrh	r3, [fp, #-10]	@ movhi
-	sxtb	r3, r3
-	orr	r3, r2, r3
-	strb	r3, [fp, #-5]
-	ldrsb	r3, [fp, #-5]
-	b	.L8
-.L12:
-	ldrh	r3, [fp, #-14]
-	and	r3, r3, #128
-	cmp	r3, #0
-	beq	.L13
-	mov	r3, #2
-	strh	r3, [fp, #-8]	@ movhi
-	ldrsh	r3, [fp, #-14]
-	asr	r3, r3, #3
-	sxth	r3, r3
-	and	r3, r3, #15
-	strh	r3, [fp, #-10]	@ movhi
-	ldrsh	r3, [fp, #-16]
-	lsl	r3, r3, #7
-	sxtb	r2, r3
-	ldrsh	r3, [fp, #-8]
-	lsl	r3, r3, #4
-	sxtb	r3, r3
-	orr	r3, r2, r3
-	sxtb	r2, r3
-	ldrh	r3, [fp, #-10]	@ movhi
-	sxtb	r3, r3
-	orr	r3, r2, r3
-	strb	r3, [fp, #-5]
-	ldrsb	r3, [fp, #-5]
-	b	.L8
-.L13:
-	ldrh	r3, [fp, #-14]
-	and	r3, r3, #64
-	cmp	r3, #0
-	beq	.L14
-	mov	r3, #1
-	strh	r3, [fp, #-8]	@ movhi
-	ldrsh	r3, [fp, #-14]
-	asr	r3, r3, #2
-	sxth	r3, r3
-	and	r3, r3, #15
-	strh	r3, [fp, #-10]	@ movhi
-	ldrsh	r3, [fp, #-16]
-	lsl	r3, r3, #7
-	sxtb	r2, r3
-	ldrsh	r3, [fp, #-8]
-	lsl	r3, r3, #4
-	sxtb	r3, r3
-	orr	r3, r2, r3
-	sxtb	r2, r3
-	ldrh	r3, [fp, #-10]	@ movhi
-	sxtb	r3, r3
-	orr	r3, r2, r3
-	strb	r3, [fp, #-5]
-	ldrsb	r3, [fp, #-5]
-	b	.L8
-.L14:
-	ldrh	r3, [fp, #-14]
-	and	r3, r3, #32
-	cmp	r3, #0
-	beq	.L15
-	mov	r3, #0
-	strh	r3, [fp, #-8]	@ movhi
-	ldrsh	r3, [fp, #-14]
-	asr	r3, r3, #1
-	sxth	r3, r3
-	and	r3, r3, #15
-	strh	r3, [fp, #-10]	@ movhi
-	ldrsh	r3, [fp, #-16]
-	lsl	r3, r3, #7
-	sxtb	r2, r3
-	ldrsh	r3, [fp, #-8]
-	lsl	r3, r3, #4
-	sxtb	r3, r3
-	orr	r3, r2, r3
-	sxtb	r2, r3
-	ldrh	r3, [fp, #-10]	@ movhi
-	sxtb	r3, r3
-	orr	r3, r2, r3
-	strb	r3, [fp, #-5]
-	ldrsb	r3, [fp, #-5]
-	b	.L8
-.L15:
-	mov	r3, #0
-.L8:
-	mov	r0, r3
-	add	sp, fp, #0
-	@ sp needed
-	ldr	fp, [sp], #4
+	and	r3, r3, #112
+	and	r0, r0, #15
+	orr	r0, r0, r3
+	orr	r0, r0, r1, lsl #7
+	uxtb	r0, r0
 	bx	lr
 	.size	codeword_compression, .-codeword_compression
-	.section	.rodata
-	.align	2
-.LC0:
-	.ascii	"Usage: %s <path/to/wav/file>\000"
-	.align	2
-.LC1:
-	.ascii	"rb\000"
-	.align	2
-.LC2:
-	.ascii	"Failed to open file\000"
-	.align	2
-.LC3:
-	.ascii	"wb\000"
-	.align	2
-.LC4:
-	.ascii	"out.wav\000"
-	.align	2
-.LC5:
-	.ascii	"Error opening output file\012\000"
-	.align	2
-.LC6:
-	.ascii	"RIFF\000"
-	.align	2
-.LC7:
-	.ascii	"WAVE\000"
-	.align	2
-.LC8:
-	.ascii	"Invalid WAV file\012\000"
-	.align	2
-.LC9:
-	.ascii	"%d\000"
-	.text
+	.section	.text.startup,"ax",%progbits
 	.align	2
 	.global	main
 	.syntax unified
@@ -340,214 +77,235 @@ codeword_compression:
 	.fpu vfpv3-d16
 	.type	main, %function
 main:
-	@ args = 0, pretend = 0, frame = 136
-	@ frame_needed = 1, uses_anonymous_args = 0
-	push	{fp, lr}
-	add	fp, sp, #4
-	sub	sp, sp, #136
-	str	r0, [fp, #-136]
-	str	r1, [fp, #-140]
-	ldr	r3, [fp, #-136]
-	cmp	r3, #2
-	beq	.L17
-	ldr	r3, [fp, #-140]
-	ldr	r3, [r3]
-	mov	r1, r3
+	@ args = 0, pretend = 0, frame = 104
+	@ frame_needed = 0, uses_anonymous_args = 0
+	push	{r4, r5, r6, r7, r8, r9, r10, fp, lr}
+	cmp	r0, #2
+	sub	sp, sp, #108
+	mov	r3, r1
+	beq	.L6
 	movw	r0, #:lower16:.LC0
+	ldr	r1, [r1]
 	movt	r0, #:upper16:.LC0
 	bl	printf
-	mov	r3, #1
-	b	.L25
-.L17:
-	ldr	r3, [fp, #-140]
-	add	r3, r3, #4
-	ldr	r3, [r3]
+	mov	r0, #1
+.L5:
+	add	sp, sp, #108
+	@ sp needed
+	pop	{r4, r5, r6, r7, r8, r9, r10, fp, pc}
+.L6:
 	movw	r1, #:lower16:.LC1
+	ldr	r0, [r3, #4]
 	movt	r1, #:upper16:.LC1
-	mov	r0, r3
 	bl	fopen
-	str	r0, [fp, #-12]
-	ldr	r3, [fp, #-12]
-	cmp	r3, #0
-	bne	.L19
-	movw	r0, #:lower16:.LC2
-	movt	r0, #:upper16:.LC2
-	bl	printf
-.L19:
+	subs	r4, r0, #0
+	beq	.L19
+.L8:
 	movw	r1, #:lower16:.LC3
-	movt	r1, #:upper16:.LC3
 	movw	r0, #:lower16:.LC4
+	movt	r1, #:upper16:.LC3
 	movt	r0, #:upper16:.LC4
 	bl	fopen
-	str	r0, [fp, #-16]
-	ldr	r3, [fp, #-16]
-	cmp	r3, #0
-	bne	.L20
-	movw	r3, #:lower16:stderr
-	movt	r3, #:upper16:stderr
-	ldr	r3, [r3]
-	mov	r2, #26
-	mov	r1, #1
-	movw	r0, #:lower16:.LC5
-	movt	r0, #:upper16:.LC5
-	bl	fwrite
-	ldr	r0, [fp, #-12]
-	bl	fclose
-	mov	r3, #0
-	b	.L25
-.L20:
-	sub	r0, fp, #80
-	ldr	r3, [fp, #-12]
+	subs	r5, r0, #0
+	beq	.L20
+	movw	r6, #18770
+	mov	r3, r4
 	mov	r2, #1
 	mov	r1, #44
+	add	r0, sp, #16
 	bl	fread
-	sub	ip, fp, #124
-	sub	lr, fp, #80
-	ldmia	lr!, {r0, r1, r2, r3}
-	stmia	ip!, {r0, r1, r2, r3}
-	ldmia	lr!, {r0, r1, r2, r3}
-	stmia	ip!, {r0, r1, r2, r3}
-	ldm	lr, {r0, r1, r2}
-	stm	ip, {r0, r1, r2}
-	sub	r3, fp, #80
-	mov	r2, #4
-	movw	r1, #:lower16:.LC6
-	movt	r1, #:upper16:.LC6
-	mov	r0, r3
-	bl	memcmp
-	mov	r3, r0
-	cmp	r3, #0
-	bne	.L21
-	sub	r3, fp, #80
-	add	r3, r3, #8
-	mov	r2, #4
-	movw	r1, #:lower16:.LC7
-	movt	r1, #:upper16:.LC7
-	mov	r0, r3
-	bl	memcmp
-	mov	r3, r0
-	cmp	r3, #0
-	beq	.L22
-.L21:
+	add	ip, sp, #16
+	add	lr, sp, #60
+	ldmia	ip!, {r0, r1, r2, r3}
+	stmia	lr!, {r0, r1, r2, r3}
+	ldmia	ip!, {r0, r1, r2, r3}
+	stmia	lr!, {r0, r1, r2, r3}
+	ldm	ip, {r0, r1, r2}
+	ldr	r3, [sp, #16]
+	movt	r6, 17990
+	cmp	r3, r6
+	stm	lr, {r0, r1, r2}
+	bne	.L10
+	movw	r3, #16727
+	ldr	r2, [sp, #24]
+	movt	r3, 17750
+	cmp	r2, r3
+	beq	.L11
+.L10:
 	movw	r3, #:lower16:stderr
+	movw	r0, #:lower16:.LC6
 	movt	r3, #:upper16:stderr
 	ldr	r3, [r3]
 	mov	r2, #17
 	mov	r1, #1
-	movw	r0, #:lower16:.LC8
-	movt	r0, #:upper16:.LC8
+	movt	r0, #:upper16:.LC6
 	bl	fwrite
-	ldr	r0, [fp, #-12]
+	mov	r0, r4
 	bl	fclose
-	ldr	r0, [fp, #-16]
+	mov	r0, r5
 	bl	fclose
-	mov	r3, #0
-	b	.L25
-.L22:
-	mov	r3, #8
-	strh	r3, [fp, #-90]	@ movhi
-	ldr	r3, [fp, #-100]
-	ldrh	r2, [fp, #-102]
-	mul	r3, r2, r3
-	str	r3, [fp, #-96]
-	ldrh	r3, [fp, #-102]
-	strh	r3, [fp, #-92]	@ movhi
-	ldr	r3, [fp, #-84]
+	mov	r0, #0
+	b	.L5
+.L11:
+	mov	ip, #8
+	ldrh	r2, [sp, #82]
+	ldr	r1, [sp, #84]
+	ldr	r3, [sp, #100]
+	mul	r1, r2, r1
 	lsr	r3, r3, #1
-	str	r3, [fp, #-84]
-	ldr	r3, [fp, #-84]
+	str	r3, [sp, #100]
 	add	r3, r3, #36
-	str	r3, [fp, #-120]
-	sub	r0, fp, #124
-	ldr	r3, [fp, #-16]
+	str	r3, [sp, #64]
+	strh	r2, [sp, #92]	@ movhi
+	mov	r3, r5
 	mov	r2, #1
+	str	r1, [sp, #88]
+	add	r0, sp, #60
 	mov	r1, #44
+	strh	ip, [sp, #94]	@ movhi
 	bl	fwrite
-	mov	r3, #0
-	str	r3, [fp, #-20]
-	mov	r3, #0
-	str	r3, [fp, #-24]
-	ldr	r3, [fp, #-44]
-	mov	r2, r3
+	ldr	r2, [sp, #52]
 	mov	r1, #0
-	ldr	r0, [fp, #-12]
+	mov	r0, r4
 	bl	fseek
-	ldr	r3, [fp, #-44]
-	mov	r1, r3
-	movw	r0, #:lower16:.LC9
-	movt	r0, #:upper16:.LC9
-	bl	printf
-	mov	r3, #0
-	str	r3, [fp, #-8]
-	b	.L23
-.L24:
-	ldr	r0, [fp, #-12]
+	ldr	r9, [sp, #56]
+	lsrs	r3, r9, #2
+	str	r3, [sp, #4]
+	beq	.L12
+	mov	r8, #65280
+	mov	r7, #32768
+	mov	r6, #0
+	movt	r8, 255
+	movt	r7, 65535
+.L13:
+	mov	r0, r4
 	bl	fgetc
-	mov	r3, r0
-	strh	r3, [fp, #-26]	@ movhi
-	ldr	r0, [fp, #-12]
+	mov	fp, r0
+	mov	r0, r4
 	bl	fgetc
-	mov	r3, r0
-	strh	r3, [fp, #-28]	@ movhi
-	ldrsh	r3, [fp, #-28]
-	lsl	r3, r3, #8
+	mov	r9, r0
+	mov	r0, r4
+	bl	fgetc
+	mov	r10, r0
+	mov	r0, r4
+	bl	fgetc
+	and	r2, r8, r9, lsl #8
+	and	r3, r8, r0, lsl #8
+	lsl	ip, r9, #6
+	lsl	r0, r0, #6
+	and	r1, r0, #8128
+	and	r2, r2, r7
+	and	ip, ip, #8128
+	and	r3, r3, r7
+	ubfx	fp, fp, #2, #14
+	ubfx	r10, r10, #2, #14
+	orr	r2, r2, ip
+	orr	r3, r3, r1
+	orr	r2, r2, fp
+	orr	r3, r3, r10
+	sxth	r2, r2
 	sxth	r3, r3
-	bic	r3, r3, #32512
-	bic	r3, r3, #255
-	sxth	r2, r3
-	ldrh	r3, [fp, #-28]
-	lsl	r3, r3, #6
-	sxth	r3, r3
-	and	r3, r3, #8128
-	sxth	r3, r3
-	orr	r3, r2, r3
-	sxth	r2, r3
-	ldrsh	r3, [fp, #-26]
-	asr	r3, r3, #2
-	sxth	r3, r3
-	orr	r3, r2, r3
-	strh	r3, [fp, #-30]	@ movhi
-	ldrsh	r3, [fp, #-30]
-	mov	r0, r3
-	bl	sign
-	mov	r3, r0
-	strh	r3, [fp, #-32]	@ movhi
-	ldrsh	r3, [fp, #-30]
-	mov	r0, r3
-	bl	magnitude
-	mov	r3, r0
-	strh	r3, [fp, #-34]	@ movhi
-	ldrsh	r2, [fp, #-32]
-	ldrsh	r3, [fp, #-34]
-	mov	r1, r2
-	mov	r0, r3
-	bl	codeword_compression
-	mov	r3, r0
-	strb	r3, [fp, #-125]
-	sub	r0, fp, #125
-	ldr	r3, [fp, #-16]
+	ubfx	ip, r2, #0, #14
+	ubfx	r0, r3, #0, #14
+	.syntax divided
+@ 45 "mu-encode.c" 1
+	CLZ lr, ip
+@ 0 "" 2
+	.arm
+	.syntax unified
+	sub	r1, lr, #26
+	cmp	r1, #0
+	sub	lr, lr, #27
+	rsblt	r1, r1, #0
+	cmp	lr, #0
+	rsblt	lr, lr, #0
+	lsl	r1, r1, #4
+	and	r1, r1, #112
+	asr	ip, ip, lr
+	lsr	r2, r2, #31
+	orr	r2, r1, r2, lsl #7
+	and	ip, ip, #15
+	orr	r2, ip, r2
+	strb	r2, [sp, #14]
+	.syntax divided
+@ 45 "mu-encode.c" 1
+	CLZ ip, r0
+@ 0 "" 2
+	.arm
+	.syntax unified
 	mov	r2, #1
-	mov	r1, #1
+	sub	r1, ip, #26
+	cmp	r1, #0
+	sub	ip, ip, #27
+	rsblt	r1, r1, #0
+	cmp	ip, #0
+	rsblt	ip, ip, #0
+	lsl	r1, r1, #4
+	and	r1, r1, #112
+	asr	ip, r0, ip
+	lsr	r3, r3, #31
+	orr	r3, r1, r3, lsl #7
+	and	ip, ip, #15
+	orr	ip, ip, r3
+	mov	r1, r2
+	mov	r3, r5
+	add	r0, sp, #14
+	strb	ip, [sp, #15]
 	bl	fwrite
-	ldr	r3, [fp, #-8]
-	add	r3, r3, #1
-	str	r3, [fp, #-8]
-.L23:
-	ldr	r3, [fp, #-40]
-	lsr	r2, r3, #1
-	ldr	r3, [fp, #-8]
-	cmp	r2, r3
-	bhi	.L24
-	ldr	r0, [fp, #-16]
+	mov	r2, #1
+	mov	r3, r5
+	add	r0, sp, #15
+	mov	r1, r2
+	add	r6, r6, r2
+	bl	fwrite
+	ldr	r3, [sp, #4]
+	cmp	r3, r6
+	bne	.L13
+.L12:
+	mov	r0, r5
 	bl	fclose
-	ldr	r0, [fp, #-12]
+	mov	r0, r4
 	bl	fclose
-	mov	r3, #0
-.L25:
-	mov	r0, r3
-	sub	sp, fp, #4
-	@ sp needed
-	pop	{fp, pc}
+	mov	r0, #0
+	b	.L5
+.L20:
+	movw	r3, #:lower16:stderr
+	movw	r0, #:lower16:.LC5
+	movt	r3, #:upper16:stderr
+	ldr	r3, [r3]
+	mov	r2, #26
+	mov	r1, #1
+	movt	r0, #:upper16:.LC5
+	bl	fwrite
+	mov	r0, r4
+	bl	fclose
+	mov	r0, r5
+	b	.L5
+.L19:
+	movw	r0, #:lower16:.LC2
+	movt	r0, #:upper16:.LC2
+	bl	printf
+	b	.L8
 	.size	main, .-main
+	.section	.rodata.str1.4,"aMS",%progbits,1
+	.align	2
+.LC0:
+	.ascii	"Usage: %s <path/to/wav/file>\000"
+	.space	3
+.LC1:
+	.ascii	"rb\000"
+	.space	1
+.LC2:
+	.ascii	"Failed to open file\000"
+.LC3:
+	.ascii	"wb\000"
+	.space	1
+.LC4:
+	.ascii	"out.wav\000"
+.LC5:
+	.ascii	"Error opening output file\012\000"
+	.space	1
+.LC6:
+	.ascii	"Invalid WAV file\012\000"
 	.ident	"GCC: (GNU) 8.2.1 20180801 (Red Hat 8.2.1-2)"
 	.section	.note.GNU-stack,"",%progbits
